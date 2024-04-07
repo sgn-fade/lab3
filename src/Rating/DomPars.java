d848c49f7545c1b1ceab535af6314e62d8f10aab
fade <dimabreusgo@gmail.com>
2024-04-07 15:03:51 +0300

    added dom parser reader( and some bugs added)

--- src/Rating/DomPars.java
+++ src/Rating/DomPars.java
@@ -1,6 +1,9 @@
 package Rating;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -9,6 +12,7 @@ import javax.xml.transform.*;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public abstract class  DomPars {
@@ -36,6 +40,34 @@ public abstract class  DomPars {
             transformer.transform(source, result);
         } catch (TransformerException e) {
             throw new RuntimeException(e);
+        }
+        try {
+            ReadDocument(filepath, DomRes.getDocument());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void ReadDocument(String filepath, Document document) throws IOException {
+        int num = document.getDocumentElement().getElementsByTagName("ethocity_group").getLength();
+
+        NodeList lstName = document.getDocumentElement().getElementsByTagName("ethocity_group");
+
+        for (int i = 0; i < num; i++) {
+            System.out.println("Ethocity: ");
+            NamedNodeMap attrs = lstName.item(i).getAttributes();
+
+            System.out.println("\t" + attrs.item(i).getNodeName() + ": " + attrs.item(i).getNodeValue().trim());
+            int babiesNum = lstName.item(i).getChildNodes().getLength();
+            NodeList babies = lstName.item(i).getChildNodes();
+
+            for(int j = 0; j < babiesNum; j++) {
+                attrs = babies.item(j).getAttributes();
+                for(int k = 0; k < attrs.getLength(); k++) {
+                    Node attr = attrs.item(k);
+                    System.out.print("\t" + attr.getNodeName() + ": " + attr.getNodeValue().trim());
+                }
+                System.out.println();
+            }
         }
     }
 }
